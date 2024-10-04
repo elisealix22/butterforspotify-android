d403d0774c82cc0240fa65dbee5a7ca34158e2f2
elisealix22 <elisealix22@gmail.com>
2024-10-04 13:26:27 -0400

    add missing todo for loading state

--- app/src/main/java/com/elisealix22/butterforspotify/music/MusicViewModel.kt
+++ app/src/main/java/com/elisealix22/butterforspotify/music/MusicViewModel.kt
@@ -1,6 +1,5 @@
 package com.elisealix22.butterforspotify.music
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.elisealix22.butterforspotify.data.playlist.FeaturedPlaylists
@@ -33,11 +32,10 @@ class MusicViewModel : ViewModel() {
         viewModelScope.launch(Dispatchers.IO) {
             playlistService.fetchFeaturedPlaylists()
                 .onStart {
-
+                    // TODO(elise): show loading
                 }
                 .catch { error ->
-                    // TODO(elise): show error
-                    Log.e("###", "ERROR", error)
+                    // TODO(elise): show error & add test
                 }
                 .collect { featuredPlaylists ->
                     _uiState.value = MusicUiState("New title", featuredPlaylists)
