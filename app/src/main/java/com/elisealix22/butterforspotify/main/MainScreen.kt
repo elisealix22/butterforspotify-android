f3d47a4f7da6ab27fb35e7d965a88c8d205bb26c
elisealix22 <elisealix22@gmail.com>
2024-10-19 19:55:43 -0400

    make player peek in portrait too

--- app/src/main/java/com/elisealix22/butterforspotify/main/MainScreen.kt
+++ app/src/main/java/com/elisealix22/butterforspotify/main/MainScreen.kt
@@ -2,6 +2,9 @@ package com.elisealix22.butterforspotify.main
 
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +16,7 @@ import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
@@ -36,6 +40,9 @@ import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffo
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -247,17 +254,18 @@ private fun PortraitUi(
 ) {
     Scaffold(
         bottomBar = {
-            Column {
-                PlayerBar(playerUiState = playerUiState)
+//            Column {
+//                PlayerBar(playerUiState = playerUiState)
                 BottomNavigationBar(navController)
-            }
+//            }
         }
     ) { innerPadding ->
-        Column {
+        Box(modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxHeight()) {
             NavHost(
                 navController = navController,
-                startDestination = BottomNavigationTabs.first().route,
-                modifier = Modifier.padding(innerPadding)
+                startDestination = BottomNavigationTabs.first().route
             ) {
                 composable<ButterRoute.Music> {
                     MusicScreen(playerUiState = playerUiState)
@@ -269,6 +277,12 @@ private fun PortraitUi(
                     )
                 }
             }
+            PlayerBar(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(horizontal = Dimen.Padding),
+                playerUiState = playerUiState
+            )
         }
     }
 }
--- app/src/main/java/com/elisealix22/butterforspotify/player/PlayerBar.kt
+++ app/src/main/java/com/elisealix22/butterforspotify/player/PlayerBar.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -35,7 +37,7 @@ import com.elisealix22.butterforspotify.ui.theme.TextStyleArtistTitle
 import com.elisealix22.butterforspotify.ui.theme.ThemeColor
 import com.elisealix22.butterforspotify.ui.theme.ThemePreview
 
-val PlayerBarHeight = 58.dp
+val PlayerBarHeight = 64.dp
 private val PlayerBarImageSize = 48.dp
 
 @Composable
@@ -45,13 +47,14 @@ fun PlayerBar(
 ) {
     Surface(
         modifier = modifier,
-        shadowElevation = 8.dp
+        shadowElevation = 8.dp,
+        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp)
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(PlayerBarHeight)
-                .padding(horizontal = Dimen.Padding)
+                .padding(horizontal = (PlayerBarHeight - PlayerBarImageSize) / 2)
         ) {
             when (playerUiState) {
                 is UiState.Success -> PlayerContent(playerUiState.data)
